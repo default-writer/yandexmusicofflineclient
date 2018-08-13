c8b22e255175e03907f1e3e3f25c8292b0fec637
hack2root <me@experimentalcommunity.org>
2018-08-13 05:35:41 +0300

    added sqlclient

--- test/sqlite3client.js
+++ test/sqlite3client.js
@@ -1,8 +1,34 @@
+const opened_state = 'opened';
+const closed_state = 'closed';
+
+let connection = {};
+
+function connectionValue(value) {
+    let d = connectionValue.d || (
+        connectionValue.d = {
+            __proto__: null,  // нет унаследованных свойств
+            enumerable: false, // не перечисляется
+            writable: false, // не перезаписывается
+            configurable: false, // не настраивается
+            value: null
+        }
+    );
+    d.value = value;
+    return d;
+}
+
+Object.defineProperty(connection, 'state', connectionValue('static'));
+
+// Если доступен метод freeze, предотвращаем добавление свойств
+// value, get, set, enumerable, writable и configurable
+// к прототипу Object
+(Object.freeze || Object)(Object.prototype);
+
 let sqlite3client = (eval) => ((data) => new Proxy(data, {
     set(obj, key, val) {
         obj[key] = val;
         eval(obj);
     }
-}))({});
+}))(connection);
 
 module.exports = sqlite3client;
--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
-var expect = require('chai').expect;
-var lazy = require('@root_admin/lazyeval/lazy');
-var sqlite3client = require('./sqlite3client');
+const expect = require('chai').expect;
+const sqlite3client = require('./sqlite3client');
+const sqlite3 = require('sqlite3').verbose();
 
 describe('open sqlite3 database', function () {
   it('should open sqlite3 database', function () {
@@ -10,10 +10,17 @@ describe('open sqlite3 database', function () {
     let b = 2;
     let c;
 
+    let open_connection = function(connection) {
+      if (connection.state === 'closed') {
+        connection.db = new object();
+        connection.state = 'open';
+      }
+    }
+
+
     // 2. ACT
-    let func = lazy((f) => c = f.a + f.b);
-    func.a = a;
-    func.b = b;
+    let client = sqlite3client(open_connection);
+    client.connection = {};
 
     // 3. ASSERT
     expect(c).to.be.equal(3);
