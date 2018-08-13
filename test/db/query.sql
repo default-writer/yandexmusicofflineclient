0dd390a96a0033755102729cfc9917f92fef50c3
hack2root <me@experimentalcommunity.org>
2018-08-13 21:26:46 +0300

    query update

--- test/db/query.sql
+++ test/db/query.sql
@@ -1,32 +1,17 @@
-select t.Title, t.CoverUri, album.TrackPosition from 
-T_Track as t,
- T_TrackAlbum as album
-where 
- album.TrackId == t.Id
-order by album.AlbumId;
-
-
-select art.TrackId ArtistrTrackId, Title, DurationMillis, Available, IsOffline, CoverUri, alb.TrackId AlbumTrackId from 
- T_Track as t,
- T_TrackAlbum as alb,
- T_TrackArtist as art
-where alb.AlbumVolume not null 
-and t.IsOffline = '1'
-and alb.TrackId == t.Id;
-
-
-/*select s.Name from T_Artist s where s.Id = tr.ArtistId) artist*/
-
-select * from (
-    select * from (
-        select * from (
-            select * from
-             T_Track as tr
-            where
-            tr.IsOffline = '1'
-        ) tr, T_TrackArtist art
-        where tr.Id == art.TrackId
-    ) tr, T_TrackAlbum ta
-    where tr.Id == ta.TrackId
-) tr, T_Album  alb
-where alb.Id = tr.AlbumId
+SELECT *
+  FROM (
+           SELECT *
+             FROM (
+                      SELECT *
+                        FROM T_Track AS tr
+                       WHERE tr.IsOffline = '1'
+                  )
+                  tr,
+                  T_TrackAlbum ta,
+                  T_TrackArtist art
+            WHERE tr.Id == art.TrackId AND 
+                  tr.Id == ta.TrackId
+       )
+       tr,
+       T_Album alb
+ WHERE alb.Id = tr.AlbumId;
