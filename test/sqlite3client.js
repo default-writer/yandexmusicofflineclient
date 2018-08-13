2fac6908228facc1e5396dbc64deef493d46baa6
hack2root <me@experimentalcommunity.org>
2018-08-13 10:05:32 +0300

    added local db files

--- test/sqlite3client.js
+++ test/sqlite3client.js
@@ -1,34 +1,81 @@
-const opened_state = 'opened';
-const closed_state = 'closed';
+const sqlite3clientexports = {};
 
-let connection = {};
+(function (exports) {
+    
+    const connection_opened = 'opened';
+    const connection_closed = 'closed';
 
-function connectionValue(value) {
-    let d = connectionValue.d || (
-        connectionValue.d = {
-            __proto__: null,  // нет унаследованных свойств
-            enumerable: false, // не перечисляется
-            writable: false, // не перезаписывается
-            configurable: false, // не настраивается
-            value: null
+    let connection = {
+        state: connection_closed
+    };
+
+    let context;
+
+    let controller = function (dbo, value) {
+        if (value === connection_opened) {
+            if (context.dbo === undefined) {
+                try {
+                    let db = dbo.open();
+                    context.dbo = db;
+                    return;
+                } catch (err) {
+                    console.log(err.message);
+                }
+            }
         }
-    );
-    d.value = value;
-    return d;
-}
+        if (value === connection_closed) {
+            if (context.dbo !== undefined) {
+                try {
+                    dbo.close(context.dbo);
+                    delete context.dbo;
+                    return;
+                } catch (err) {
+                    console.log(err.message);
+                }
+            }
+        }
+    };
 
-Object.defineProperty(connection, 'state', connectionValue('static'));
+    Object.defineProperty(connection, 'open', {
+        __proto__: null, // нет унаследованных свойств
+        enumerable: false, // не перечисляется
+        writable: false, // не перезаписывается
+        configurable: false, // не настраивается
+        value: function (state) {
+            context = state ? state : connection;
+            this.state = connection_opened;
+            context = undefined;
+        }
+    });
 
-// Если доступен метод freeze, предотвращаем добавление свойств
-// value, get, set, enumerable, writable и configurable
-// к прототипу Object
-(Object.freeze || Object)(Object.prototype);
+    Object.defineProperty(connection, 'close', {
+        __proto__: null, // нет унаследованных свойств
+        enumerable: false, // не перечисляется
+        writable: false, // не перезаписывается
+        configurable: false, // не настраивается
+        value: function (state) {
+            context = state ? state : connection;
+            this.state = connection_closed;
+            context = undefined;
+        }
+    });
 
-let sqlite3client = (eval) => ((data) => new Proxy(data, {
-    set(obj, key, val) {
-        obj[key] = val;
-        eval(obj);
-    }
-}))(connection);
+    // Если доступен метод freeze, предотвращаем добавление свойств
+    // value, get, set, enumerable, writable и configurable
+    // к прототипу Object
+    (Object.freeze || Object)(Object.prototype);
 
-module.exports = sqlite3client;
+    let Database = (db) => new Proxy(connection, {
+        set(obj, key, value) {
+            if (key === 'state') {
+                controller(db, value);
+                obj[key] = value;
+            }
+        }
+    });
+
+    exports.Database = Database;
+
+})(sqlite3clientexports);
+
+module.exports.Database = sqlite3clientexports.Database;
--- test/test.js
+++ test/test.js
@@ -1,28 +1,378 @@
 const expect = require('chai').expect;
-const sqlite3client = require('./sqlite3client');
+
+const dbo = require('./sqlite3client').Database;
 const sqlite3 = require('sqlite3').verbose();
 
-describe('open sqlite3 database', function () {
-  it('should open sqlite3 database', function () {
+const sqlite3Database = dbo({
+  open: () => new sqlite3.Database('./test/db/musicdb_3f0b48061129292a1244c536233a24ec.sqlite', (err) => {
+    if (err) {
+      throw err;
+    }
+  }),
+  close: (db) => db.close((err) => {
+    if (err) {
+      throw err;
+    }
+  })
+});
+const global_connection = {};
+
+// 1 1`2 2`
+
+describe('open sqlite3 database 1', function () {
+  it('should open sqlite3 database 1', function () {
 
     // 1. ARRANGE
-    let a = 1;
-    let b = 2;
-    let c;
-
-    let open_connection = function(connection) {
-      if (connection.state === 'closed') {
-        connection.db = new object();
-        connection.state = 'open';
-      }
-    }
-
+    let database = sqlite3Database;
 
     // 2. ACT
-    let client = sqlite3client(open_connection);
-    client.connection = {};
+    database.open();
 
     // 3. ASSERT
-    expect(c).to.be.equal(3);
+    expect(database.dbo).not.to.be.equal(undefined);
+  });
+});
+
+describe('close sqlite3 database 1', function () {
+  it('should close sqlite3 database 1', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+
+    // 2. ACT
+    database.close();
+
+    // 3. ASSERT
+    expect(database.db).to.be.equal(undefined);
+  });
+});
+
+describe('open sqlite3 database 2', function () {
+  it('should open sqlite3 database 2', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+    let connection = global_connection;
+
+    // 2. ACT
+    database.open(connection);
+
+    // 3. ASSERT
+    expect(connection.dbo).not.to.be.equal(undefined);
+  });
+});
+
+describe('close sqlite3 database 2', function () {
+  it('should close sqlite3 database 2', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+    let connection = global_connection;
+
+    // 2. ACT
+    database.close(connection);
+
+    // 3. ASSERT
+    expect(connection.dbo).to.be.equal(undefined);
+  });
+});
+
+// 1 2 1`2`
+
+describe('open sqlite3 database 1', function () {
+  it('should open sqlite3 database 1', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+
+    // 2. ACT
+    database.open();
+
+    // 3. ASSERT
+    expect(database.dbo).not.to.be.equal(undefined);
+  });
+});
+
+describe('open sqlite3 database 2', function () {
+  it('should open sqlite3 database 2', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+    let connection = global_connection;
+
+    // 2. ACT
+    database.open(connection);
+
+    // 3. ASSERT
+    expect(connection.dbo).not.to.be.equal(undefined);
+  });
+});
+
+describe('close sqlite3 database 1', function () {
+  it('should close sqlite3 database 1', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+
+    // 2. ACT
+    database.close();
+
+    // 3. ASSERT
+    expect(database.db).to.be.equal(undefined);
+  });
+});
+
+describe('close sqlite3 database 2', function () {
+  it('should close sqlite3 database 2', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+    let connection = global_connection;
+
+    // 2. ACT
+    database.close(connection);
+
+    // 3. ASSERT
+    expect(connection.dbo).to.be.equal(undefined);
+  });
+});
+
+// 1 2 2` 1`
+
+describe('open sqlite3 database 1', function () {
+  it('should open sqlite3 database 1', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+
+    // 2. ACT
+    database.open();
+
+    // 3. ASSERT
+    expect(database.dbo).not.to.be.equal(undefined);
+  });
+});
+
+describe('open sqlite3 database 2', function () {
+  it('should open sqlite3 database 2', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+    let connection = global_connection;
+
+    // 2. ACT
+    database.open(connection);
+
+    // 3. ASSERT
+    expect(connection.dbo).not.to.be.equal(undefined);
+  });
+});
+
+describe('close sqlite3 database 2', function () {
+  it('should close sqlite3 database 2', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+    let connection = global_connection;
+
+    // 2. ACT
+    database.close(connection);
+
+    // 3. ASSERT
+    expect(connection.dbo).to.be.equal(undefined);
+  });
+});
+
+describe('close sqlite3 database 1', function () {
+  it('should close sqlite3 database 1', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+
+    // 2. ACT
+    database.close();
+
+    // 3. ASSERT
+    expect(database.db).to.be.equal(undefined);
+  });
+});
+
+// 2 2`1 1`
+
+describe('open sqlite3 database 2', function () {
+  it('should open sqlite3 database 2', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+    let connection = global_connection;
+
+    // 2. ACT
+    database.open(connection);
+
+    // 3. ASSERT
+    expect(connection.dbo).not.to.be.equal(undefined);
+  });
+});
+
+describe('close sqlite3 database 2', function () {
+  it('should close sqlite3 database 2', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+    let connection = global_connection;
+
+    // 2. ACT
+    database.close(connection);
+
+    // 3. ASSERT
+    expect(connection.dbo).to.be.equal(undefined);
+  });
+});
+
+describe('open sqlite3 database 1', function () {
+  it('should open sqlite3 database 1', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+
+    // 2. ACT
+    database.open();
+
+    // 3. ASSERT
+    expect(database.dbo).not.to.be.equal(undefined);
+  });
+});
+
+describe('close sqlite3 database 1', function () {
+  it('should close sqlite3 database 1', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+
+    // 2. ACT
+    database.close();
+
+    // 3. ASSERT
+    expect(database.db).to.be.equal(undefined);
+  });
+});
+
+// 2 1 2`1`
+
+describe('open sqlite3 database 2', function () {
+  it('should open sqlite3 database 2', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+    let connection = global_connection;
+
+    // 2. ACT
+    database.open(connection);
+
+    // 3. ASSERT
+    expect(connection.dbo).not.to.be.equal(undefined);
+  });
+});
+
+describe('open sqlite3 database 1', function () {
+  it('should open sqlite3 database 1', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+
+    // 2. ACT
+    database.open();
+
+    // 3. ASSERT
+    expect(database.dbo).not.to.be.equal(undefined);
+  });
+});
+
+describe('close sqlite3 database 2', function () {
+  it('should close sqlite3 database 2', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+    let connection = global_connection;
+
+    // 2. ACT
+    database.close(connection);
+
+    // 3. ASSERT
+    expect(connection.dbo).to.be.equal(undefined);
+  });
+});
+
+describe('close sqlite3 database 1', function () {
+  it('should close sqlite3 database 1', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+
+    // 2. ACT
+    database.close();
+
+    // 3. ASSERT
+    expect(database.db).to.be.equal(undefined);
+  });
+});
+
+// 2 1 1`2`
+
+describe('open sqlite3 database 2', function () {
+  it('should open sqlite3 database 2', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+    let connection = global_connection;
+
+    // 2. ACT
+    database.open(connection);
+
+    // 3. ASSERT
+    expect(connection.dbo).not.to.be.equal(undefined);
+  });
+});
+
+describe('open sqlite3 database 1', function () {
+  it('should open sqlite3 database 1', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+
+    // 2. ACT
+    database.open();
+
+    // 3. ASSERT
+    expect(database.dbo).not.to.be.equal(undefined);
+  });
+});
+
+describe('close sqlite3 database 1', function () {
+  it('should close sqlite3 database 1', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+
+    // 2. ACT
+    database.close();
+
+    // 3. ASSERT
+    expect(database.db).to.be.equal(undefined);
+  });
+});
+
+describe('close sqlite3 database 2', function () {
+  it('should close sqlite3 database 2', function () {
+
+    // 1. ARRANGE
+    let database = sqlite3Database;
+    let connection = global_connection;
+
+    // 2. ACT
+    database.close(connection);
+
+    // 3. ASSERT
+    expect(connection.dbo).to.be.equal(undefined);
   });
 });
