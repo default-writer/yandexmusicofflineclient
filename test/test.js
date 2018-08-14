13fd9a0e9f421724db28427a514d24a171e6cb8b
hack2root <me@experimentalcommunity.org>
2018-08-14 13:29:53 +0300

    sql simplifications

--- test/test.js
+++ test/test.js
@@ -425,35 +425,36 @@ describe('open sqlite3 database', function () {
   ORDER BY alb.Id, position);
     */
     connection.sql = `
-    SELECT DISTINCT *
-    FROM (
-             SELECT tr.Id track,
-                    alb.CoverUri url,
-                    alb.Title titile,
-                    position,
-                    name,
-                    alb.ArtistsString album,
-                    alb.Year year,
-                    alb.AlbumVersion,
-                    alb.GenreId genre
-               FROM (
-                        SELECT Id,
-                               Title name,
-                               ta.TrackPosition position,
-                               ta.AlbumId
-                          FROM T_Track AS tr
-                               INNER JOIN
-                               T_TrackAlbum ta ON tr.Id = ta.TrackId
-                               INNER JOIN
-                               T_TrackArtist art ON tr.Id = art.TrackId
-                         WHERE tr.IsOffline = '1'
-                    )
-                    tr,
-                    T_Album alb
-              WHERE alb.Id = tr.AlbumId
-              ORDER BY alb.Id,
-                       position
-         );`;
+    SELECT DISTINCT tr.TrackId file,
+    alb.CoverUri url,
+    alb.Title titile,
+    tr.TrackPosition position,
+    tr.Title title,
+    art.Name || ' - ' || alb.ArtistsString autor,
+    alb.Year year,
+    alb.AlbumVersion album,
+    alb.GenreId genre
+FROM (
+SELECT Title,
+      art.ArtistId,
+      ta.TrackId,
+      ta.TrackPosition,
+      ta.AlbumId
+ FROM T_Track AS tr
+      INNER JOIN
+      T_TrackAlbum ta ON tr.Id = ta.TrackId
+      INNER JOIN
+      T_TrackArtist art ON tr.Id = art.TrackId
+WHERE tr.IsOffline = '1'
+)
+tr
+INNER JOIN
+T_Album alb ON tr.AlbumId = alb.Id
+INNER JOIN
+T_Artist art ON tr.ArtistId = art.Id
+ORDER BY alb.Id,
+position;
+`;
     database.query(connection);
 
     // 3. ASSERT
